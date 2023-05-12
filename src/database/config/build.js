8976c592702856dfa306de82450b51cf5a984323
Eng-Mohammed-Sbeata <majd86787@gmail.com>
2023-05-12 06:03:54 +0300

    finish all Project

--- src/database/config/build.js
+++ src/database/config/build.js
@@ -6,7 +6,5 @@ const Build = () => {
   const sql = readFileSync(join(__dirname, 'build.sql')).toString();
   return connection.query(sql);
 };
-if (process.env.NODE_ENV === 'development') {
-  Build();
-}
+
 module.exports = Build;
